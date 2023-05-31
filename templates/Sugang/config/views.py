d7edd18f83f45c70ffdbedcf883bc3e5a8f36dc1
hckang17 <hckang17@naver.com>
2023-06-01 03:22:58 +0900

    14th test commit

--- templates/Sugang/config/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.http import JsonResponse
-
-def welcome_page(request):
-    return render(request, 'first_page.html')
--- templates/Sugang/sugang/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SugangConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sugang'
